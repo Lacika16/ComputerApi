84b970eb8e388e49b5c5f99f40df4192bf31386b
Lacika16 <klimal@kkszki.hu>
2024-11-26 13:47:51 +0100

    day finish

--- Computer1Api/Controllers/ComputerController.cs
+++ Computer1Api/Controllers/ComputerController.cs
@@ -34,24 +34,33 @@ namespace Computer1Api.Controllers
             {
                 await computerContext.Comps.AddAsync(comp);
                 await computerContext.SaveChangesAsync();
-
                 return StatusCode(201, comp);
             }
-            return BadRequest(new { messages = "Hiba az objektum megadásánál" });
+            return BadRequest(new { messages = "Hiba az objektum megadásnál." });
         }
-
         [HttpGet]
-
         public async Task<ActionResult<Comp>> Get()
         {
-            return Ok(computerContext.Comps.Select(x=>new { x.Brand, x.Type, x.Memory, x.Os.Name }).ToListAsync());
+            return Ok(await computerContext.Comps.Select(x => new { x.Brand, x.Type, x.Memory }).ToListAsync());
         }
 
         [HttpGet("numberOfComputers")]
         public async Task<ActionResult> GetNumberOfComputers()
         {
-            var comps = await computerContext.Comps.ToListAsync();
-            return Ok(new { message = comps.Count() } );
+            var comps = await computerContext.Comps.ToArrayAsync();
+            return Ok(new { message = "Sikeres", count = comps.Count() });
+        }
+
+        [HttpGet("allWindowsOsComputer")]
+        public async Task<ActionResult<Comp>> GetAllWindowsOsComputer()
+        {
+            return Ok(await computerContext.Comps.Where(x => x.Os.Name.Contains("windows")).Select(x => new { x.Os.Name }).ToListAsync());
+        }
+
+        [HttpGet("osOrderWithDescendant")]
+        public async Task<ActionResult<O>> GetOsOrderDescendant()
+        {
+            return Ok(await computerContext.Osystem.OrderByDescending(x => x.Name).ToListAsync());
         }
     }
 }
